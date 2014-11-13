591037dd1afbed95f27715df6ed9c57578c8abbb
Kerrigan29a <kerrigan29a@gmail.com>
2014-11-13 10:46:54 +0100

    First literate version plus noweb.py integration

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 CPPFLAGS	?= -DDEBUG
 CFLAGS		?= -Wall -Werror
 
-all: mlp
+all: mlp README.md
 
 mlp.o: mlp.c
+
+mlp.c: mlp.nw
+	./noweb.py/noweb.py -o mlp.c tangle -R mlp.c mlp.nw
+
+README.md: mlp.nw
+	./noweb.py/noweb.py -o README.md weave --default-code-syntax=c mlp.nw
 
 clean:
 	-@$(RM) mlp mlp.o
 
 distclean: clean
+	-@$(RM) mlp.c README.md
--- mlp.c
+++ mlp.c
@@ -3,10 +3,6 @@
  * Copyright (c) 2014  Javier Escalada Gómez (kerrigan29a@gmail.com)
  */
 
-/*
- * For explanations see:  http://www.cs.bham.ac.uk/~jxb/NN/nn.html
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -20,9 +16,11 @@
 
 #define MAX_EPOCH           1000000
 
+
+
 /*
- * This is a good approximation to a uniform random distribution in the
- * interval [0, 1):
+ * This is a good approximation to a uniform random distribution in the interval
+ * [0, 1):
  * http://www.thinkage.ca/english/gcos/expl/c/lib/rand.html
  */
 #define normalized_rand() ((double) rand() / (RAND_MAX + 1))
@@ -80,8 +78,10 @@ int main(void)
     double DeltaWeightIH[NUM_INPUT_UNITS + 1][NUM_HIDDEN_UNITS + 1];
     double DeltaWeightHO[NUM_HIDDEN_UNITS + 1][NUM_OUTPUT_UNITS + 1];
     double Error;
+
     double eta = 0.5;
     double alpha = 0.9;
+
     double smallwt = 0.5;
 
     /* Seed random number function */
@@ -104,9 +104,9 @@ int main(void)
     }
 
     /* Train */
-    for( epoch = 0; epoch < MAX_EPOCH; epoch++ ) {
+    for(epoch=0; epoch<MAX_EPOCH; epoch++) {
 
-        /* Randomize order of individuals */
+        /* Randomize the order of patterns */
         for( p = 1; p <= NumPattern; p++ ) {
             ranpat[p] = p;
         }
@@ -118,72 +118,94 @@ int main(void)
         }
 
         Error = 0.0;
-        /* Repeat for all the training patterns */
-        for( np = 1; np <= NumPattern; np++ ) {
+        /* Train with each pattern */
+        for(np=1; np<=NumPattern; np++) {
             p = ranpat[np];
 
             /* Compute hidden unit activations */
-            for( j = 1; j <= NumHidden; j++ ) {
+            for (j=1; j<=NumHidden; j++ ) {
                 SumH[p][j] = WeightIH[0][j];
-                for( i = 1; i <= NumInput; i++ ) {
+                for( i=1; i<=NumInput; i++ ) {
                     SumH[p][j] += Input[p][i] * WeightIH[i][j];
                 }
-                Hidden[p][j] = 1.0/(1.0 + exp(-SumH[p][j]));
+
+                Hidden[p][j] = 1.0 / (1.0 + exp(-SumH[p][j]));
             }
 
+            /* Here we compute the error */
             /* Compute output unit activations and errors */
-            for( k = 1; k <= NumOutput; k++ ) {
+            for(k=1; k<=NumOutput; k++) {
                 SumO[p][k] = WeightHO[0][k];
                 for( j = 1; j <= NumHidden; j++ ) {
                     SumO[p][k] += Hidden[p][j] * WeightHO[j][k];
                 }
-                /* Sigmoidal Outputs */
-                Output[p][k] = 1.0/(1.0 + exp(-SumO[p][k]));
-                /* Linear Outputs */
+
+                /* Sigmoidal outputs */
+                Output[p][k] = 1.0 / (1.0 + exp(-SumO[p][k]));
+                /* and */
+                /* Sum squared error */
+                Error += 0.5 * (Target[p][k] - Output[p][k]) * (Target[p][k] - Output[p][k]);
+                /* and */
+                /* Delta for sigmoidal outputs and sum squared error */
+                DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]);
+
+                /* or (forget about this at the moment of reading) */
+                /* Linear outputs */
                 // Output[p][k] = SumO[p][k];
-
-                Error += 0.5 * (Target[p][k] - Output[p][k]) * (Target[p][k] - Output[p][k]);   /* SSE */
-                /* Cross-Entropy Error */
-                // Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) );
-
-                /* Sigmoidal Outputs */
-                DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]);   /* SSE */
-                /* Sigmoidal Outputs, Cross-Entropy Error */
+                /* and */
+                /* Delta for linear outputs and sum squared error */
                 // DeltaO[k] = Target[p][k] - Output[p][k];
-                /* Linear Outputs */
-                // DeltaO[k] = Target[p][k] - Output[p][k]; /* SSE */
+
+                /* or (forget about this at the moment of reading) */
+                /* Compute sigmoidal output */
+                /* and */
+                /* Cross-entropy error */
+                // Error -= (Target[p][k] * log(Output[p][k]) + (1.0 - Target[p][k]) * log(1.0 - Output[p][k]));
+                /* and */
+                /* Delta for sigmoidal outputs and cross-entropy error */
+                // DeltaO[k] = Target[p][k] - Output[p][k];
             }
 
             /* Back-propagate errors to hidden layer */
-            for( j = 1; j <= NumHidden; j++ ) {
+            for(j=1; j<=NumHidden; j++) {
                 SumDOW[j] = 0.0;
-                for( k = 1; k <= NumOutput; k++ ) {
+                for(k=1; k<=NumOutput; k++) {
                     SumDOW[j] += WeightHO[j][k] * DeltaO[k];
                 }
+
                 DeltaH[j] = SumDOW[j] * Hidden[p][j] * (1.0 - Hidden[p][j]);
             }
 
             /* Update weights WeightIH */
-            for( j = 1; j <= NumHidden; j++ ) {
-                DeltaWeightIH[0][j] = eta * DeltaH[j] + alpha * DeltaWeightIH[0][j];
+            for(j=1; j<=NumHidden; j++) {
+                DeltaWeightIH[0][j] = eta * DeltaH[j]
+                    + alpha * DeltaWeightIH[0][j];
+
                 WeightIH[0][j] += DeltaWeightIH[0][j];
-                for( i = 1; i <= NumInput; i++ ) {
-                    DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j] + alpha * DeltaWeightIH[i][j];
+
+                for(i=1; i<=NumInput; i++) {
+                    DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j]
+                        + alpha * DeltaWeightIH[i][j];
                     WeightIH[i][j] += DeltaWeightIH[i][j];
                 }
             }
 
             /* Update weights WeightHO */
-            for( k = 1; k <= NumOutput; k ++ ) {
-                DeltaWeightHO[0][k] = eta * DeltaO[k] + alpha * DeltaWeightHO[0][k];
+            for(k=1; k<=NumOutput; k++) {
+                DeltaWeightHO[0][k] = eta * DeltaO[k]
+                    + alpha * DeltaWeightHO[0][k];
+
                 WeightHO[0][k] += DeltaWeightHO[0][k];
-                for( j = 1; j <= NumHidden; j++ ) {
-                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DeltaWeightHO[j][k];
+
+                for(j=1; j<=NumHidden; j++) {
+                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k]
+                        + alpha * DeltaWeightHO[j][k];
                     WeightHO[j][k] += DeltaWeightHO[j][k];
                 }
             }
         }
 
+        /* Show computation state each few epochs */
         if( epoch % 1000 == 0 ) {
             fprintf(stdout, "\rEpoch %-6d :   Error = %f", epoch, Error);
         }
@@ -217,5 +239,3 @@ print_result:
     fprintf(stdout, "\n\nDone!\n");
     return 0;
 }
-
-/*******************************************************************************/
